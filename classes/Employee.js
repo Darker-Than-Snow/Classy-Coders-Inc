40c848f79440fabca2e1ac4b6eb6a3ea35a4a89a
Tekena Onugha <okwudilionugha@gmail.com>
2025-02-20 15:08:16 +0000

    First commit

--- classes/Employee.js
+++ classes/Employee.js
@@ -1,10 +1,51 @@
 class Employee {
-
-
-
-    
-}
-
+    constructor(name, position, salary) {
+      this.name = name;
+      this.position = position;
+      this.#salary = salary;
+      this.#isHired = true;
+      Employee.#allEmployees.push(this);
+    }
+  
+    #salary;
+    #isHired;
+    static #allEmployees = [];
+  
+    getSalary() {
+      return this.#salary;
+    }
+  
+    setSalary(amount) {
+      this.#salary = amount;
+    }
+  
+    getStatus() {
+      return this.#isHired;
+    }
+  
+    setStatus(command) {
+      if (command === "hire") {
+        this.#isHired = true;
+      } else if (command === "fire") {
+        this.#isHired = false;
+      }
+    }
+  
+    static getEmployees() {
+      return Employee.#allEmployees;
+    }
+  
+    static getTotalPayroll() {
+      return Employee.#allEmployees.reduce((total, employee) => {
+        if (employee.getStatus()) {
+          return total + employee.getSalary();
+        }
+        return total;
+      }, 0);
+    }
+  }
+  
+  
 module.exports = {
     Employee,
 }
--- classes/Manager.js
+++ classes/Manager.js
@@ -1,8 +1,21 @@
-class Manager {
+const Employee = require('./Employee');
 
+class Manager extends Employee {
+  constructor(name, position, department) {
+    super(name, position);
+    this.department = department;
+    this.#employeesManaged = [];
+  }
 
+  #employeesManaged;
 
-    
+  getEmployeesManaged() {
+    return this.#employeesManaged;
+  }
+
+  addEmployeeManaged(employee) {
+    this.#employeesManaged.push(employee);
+  }
 }
 
 module.exports = {
