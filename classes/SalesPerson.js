8647e4d95c7846acd94a779f5c2e6972ccd11788
Tekena Onugha <okwudilionugha@gmail.com>
2025-02-20 15:11:17 +0000

    second commit

--- classes/SalesPerson.js
+++ classes/SalesPerson.js
@@ -1,8 +1,21 @@
-class SalesPerson {
+const Employee = require('./Employee');
 
+class SalesPerson extends Employee {
+  constructor(name, position) {
+    super(name, position);
+    this.clients = [];
+    this.#totalSales = 0;
+  }
 
+  #totalSales;
 
-    
+  getSalesNumbers() {
+    return this.#totalSales;
+  }
+
+  makeSale(amount) {
+    this.#totalSales += amount;
+  }
 }
 
 module.exports = {
--- classes/SoftwareEngineer.js
+++ classes/SoftwareEngineer.js
@@ -1,9 +1,22 @@
-class SoftwareEngineer {
+const Employee = require('./Employee');
 
+class SoftwareEngineer extends Employee {
+  constructor(name, position) {
+    super(name, position);
+    this.#programmingLanguages = [];
+  }
 
+  #programmingLanguages;
 
-    
+  getProgrammingLanguages() {
+    return this.#programmingLanguages;
+  }
+
+  setProgrammingLanguage(language) {
+    this.#programmingLanguages.push(language);
+  }
 }
+
 
 module.exports = {
     SoftwareEngineer,
